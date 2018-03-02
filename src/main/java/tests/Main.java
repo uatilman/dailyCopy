fc575574ddb510843bfe8b68b4ff647ef1638972
uatilman <uatilman@gmail.com>
2018-03-02 16:36:45 +0300

    betta 1.0 done

--- src/main/java/tests/Main.java
+++ src/main/java/tests/Main.java
@@ -12,45 +12,70 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.logging.*;
 import java.util.stream.Collectors;
+
+import static java.util.logging.Level.*;
+import static java.util.logging.Level.SEVERE;
 
 
 public class Main {
-    private static final Path SRC = Paths.get("srcFiles").toAbsolutePath();
-    private static final Path DST = Paths.get("dstFiles").toAbsolutePath();
+    public static final Logger LOGGER = Logger.getLogger(MyFile.class.getName());
+    private static final Path SRC = Paths.get("C:\\Users\\usr-mbk00066\\Desktop\\srcFiles").toAbsolutePath();
+    private static final Path DST = Paths.get("C:\\Users\\usr-mbk00066\\Desktop\\dstFiles").toAbsolutePath();
+    private static final Path TRASH_PATH = Paths.get("C:\\Users\\usr-mbk00066\\Desktop\\dstFiles\\trash").toAbsolutePath();
 
-    public static void main(String[] args) throws Exception {
-       MyFile src = new MyFile(SRC, SRC);
-       MyFile dst = new MyFile(DST, DST);
+    public static void main(String[] args) {
+        initLogger();
 
-          src.sync(dst);
+        MyFile src;
+        MyFile dst;
 
-//        List<MyFile> myFilesDst = MyFile.getFileTreePaths(DST, false);
-//        myFilesSrc.sort(Comparator.comparing(MyFile::getPath));
-//        myFilesDst.sort(Comparator.comparing(MyFile::getPath));
-//
-//
-//        System.out.println(myFilesSrc);
+        while (true) {
+            try {
+                Main.LOGGER.log(INFO, "\n===========================\n" + new SimpleDateFormat("YYYY.MM.dd_H.mm.s").format(Calendar.getInstance().getTime()) + "\n");
+                src = new MyFile(SRC, SRC, TRASH_PATH);
+                dst = new MyFile(DST, DST, TRASH_PATH);
+                src.sync(dst);
+
+                Thread.sleep(60000);
+
+            } catch (Exception e) {
+                LOGGER.log(WARNING, "", e);
+                break;
+            }
+        }
+
 
     }
 
-    private static void streamTest() {
-        List<Path> paths = new ArrayList<>();
-        List<Path> paths1 = new ArrayList<>();
-        paths.addAll(paths.stream().filter(path -> Files.isDirectory(path)).collect(Collectors.toList()));
-        paths.forEach(path -> System.out.println(path.getFileName()));
+    private static void initLogger() {
+        LOGGER.setLevel(ALL);
+        LOGGER.setUseParentHandlers(false);
+        try {
+
+            Handler fileHandlerException = new FileHandler("exception.log");
+            fileHandlerException.setFormatter(new SimpleFormatter());
+            fileHandlerException.setLevel(WARNING);
+            LOGGER.addHandler(fileHandlerException);
+
+
+            Handler fileHandlerInfo = new FileHandler("info.log");
+            fileHandlerInfo.setFormatter(new SimpleFormatter());
+            fileHandlerInfo.setLevel(CONFIG);
+            fileHandlerInfo.setFilter(record -> record.getLevel().equals(INFO));
+            LOGGER.addHandler(fileHandlerInfo);
+
+
+            Handler consoleHandler = new ConsoleHandler();
+            consoleHandler.setLevel(SEVERE);
+            LOGGER.addHandler(consoleHandler);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-
-    private static Map<String, Object> getAttributes(Path path) throws IOException {
-        BasicFileAttributes attr;
-        attr = Files.readAttributes(path, BasicFileAttributes.class, LinkOption.NOFOLLOW_LINKS);
-//        System.out.println("Creation time: " + attr.creationTime());
-//        System.out.println("Last access time: " + attr.lastAccessTime());
-//        System.out.println("Last modified time: " + attr.lastModifiedTime());
-        return Files.readAttributes(path, "*", LinkOption.NOFOLLOW_LINKS);
-    }
-
 
 }
--- src/main/java/tests/MyFile.java
+++ src/main/java/tests/MyFile.java
@@ -1,34 +1,48 @@
 package tests;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.nio.file.*;
 import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
+import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.WARNING;
 
 public class MyFile implements Serializable, Comparable {
     private Path path;
     private List<MyFile> childList;
     private long time;
     private Path absPath;
+    private Path trash;
+
 
     private boolean isDir;
 
-    public MyFile(Path path, Path root) {
 
+    public MyFile(Path path, Path root, Path trash) {
+        this.trash = trash;
         this.absPath = path;
         this.path = root.toAbsolutePath().relativize(path.toAbsolutePath());
 
         if (Files.isDirectory(path)) {
             this.isDir = true;
-            try {
-                this.childList = Files.list(path).filter(MyFile::isHidden).map(path1 -> new MyFile(path1, root)).sorted().collect(Collectors.toList());
+            try (Stream<Path> str = Files.list(path)) {
+                this.childList = str
+                        .filter(path2 -> !path2.getFileName().toString().equals(trash.getFileName().toString()))
+                        .map(path1 -> new MyFile(path1, root, trash))
+                        .sorted()
+                        .collect(Collectors.toList());
             } catch (IOException e) {
+                Main.LOGGER.log(WARNING, "", e);
                 e.printStackTrace();
             }
         } else {
@@ -39,73 +53,95 @@ public class MyFile implements Serializable, Comparable {
         try {
             this.time = Files.getLastModifiedTime(path, NOFOLLOW_LINKS).toMillis();
         } catch (IOException e) {
+            Main.LOGGER.log(WARNING, "", e);
             e.printStackTrace();
         }
     }
 
-    private static boolean isHidden(Path path) {
-        return !path.getFileName().toString().startsWith("~");
-    }
+    public void sync(MyFile dst) {
+        try {
+            List<MyFile> removeList = new ArrayList<>();
+            for (int i = 0; i < childList.size(); i++) {
+                List<MyFile> dstChildList = dst.childList;
+                MyFile currentSrc = childList.get(i);
+                int index = currentSrc.containByName(dstChildList); // индекс ткущего элемента в целевом списке
+                MyFile currentDst = index >= 0 ? dstChildList.get(index) : null;
 
-    public void sync(MyFile dst) throws IOException {
-        List<MyFile> removeList = new ArrayList<>();
+                Path srcPath = currentSrc.absPath;
+                Path dstPath = currentDst != null ?
+                        currentDst.absPath :
+                        Paths.get(dst.absPath.toString(), currentSrc.path.toString());
 
-        for (int i = 0; i < childList.size(); i++) {
-            List<MyFile> dstChildList = dst.childList;
-            MyFile currentSrc = childList.get(i);
-            int index = currentSrc.containByName(dstChildList); // индекс ткущего элемента в целевом списке
-            MyFile currentDst = index >= 0 ? dstChildList.get(index) : null;
+                if (currentSrc.isDir) { // если папка
+                    if (currentDst != null) {// если папка существует
+                        currentSrc.sync(currentDst);
+                    } else { //если имени в целевом списке нет - копируем папку целиком
+                        Files.createDirectory(dstPath);
+                        Main.LOGGER.log(INFO, dstPath + " - папка не найдена. Папка создана заново и начато копирования");
+                        FileUtils.copyDirectory(srcPath.toFile(), dstPath.toFile());
+                    }
+                    Files.setLastModifiedTime(dstPath, FileTime.fromMillis(currentSrc.time));
 
-            Path srcPath = currentSrc.absPath;
-            Path dstPath = currentDst != null ?
-                    currentDst.absPath :
-                    Paths.get(dst.absPath.toString(), currentSrc.path.toString());
-            // TODO: 28.02.2018 отдельный блок вначале синхронизации с проверкой на полное совпадение - логично только для файлов, т.к. сравнение папок по дате и названию ненадежно
-            // TODO: 28.02.2018 перенос в корзину вместо удаления - если перенос без архива, операция быстрая т.к. файл меняется не сильно.
-
-            if (currentSrc.isDir) { // если папка
-                if (currentDst != null) {// если папка существует
-                    currentSrc.sync(currentDst);
-                } else { //если имени в целевом списке нет - копируем папку целиком
-                    Files.createDirectory(dstPath);
-                    FileUtils.copyDirectory(srcPath.toFile(), dstPath.toFile());
-                }
-                Files.setLastModifiedTime(dstPath, FileTime.fromMillis(currentSrc.time));
-
-            } else { // если файл
-                if (!dstChildList.contains(currentSrc)) { // если файлы не одинаковые
-                    if (currentDst != null) {// если совпадают имена
-                        if (currentSrc.isNewer(currentDst)) { // если исходный файл новее
-                            Files.delete(dstPath);
-                            FileUtils.copyFile(srcPath.toFile(), dstPath.toFile());
-                        } else { //новее файл в резервной копии
-                            // этого неможет быть
-                            System.err.println(
-                                    "В резервном хранилище файл новее. " +
-                                            "\n\t Исхожный файл: " + currentSrc + "-  " + FileTime.fromMillis(currentSrc.time) +
-                                            "\n\t Резервный файл: " + currentDst + "-  " + FileTime.fromMillis(currentDst.time));
+                } else { // если файл
+                    if (!dstChildList.contains(currentSrc)) { // если файлы не одинаковые
+                        if (currentDst != null) {// если совпадают имена
+                            if (currentSrc.isNewer(currentDst)) { // если исходный файл новее
+                                moveOnce(currentDst);
+                                Main.LOGGER.log(INFO, currentDst + " перенесен в корзину");
+                                FileUtils.copyFile(srcPath.toFile(), dstPath.toFile());
+                                Main.LOGGER.log(INFO, currentDst + " обновлен");
+                            } else { //новее файл в резервной копии
+                                Main.LOGGER.log(INFO, "В резервном хранилище файл новее. " +
+                                        "\n\t Исхожный файл: " + currentSrc + "-  " + FileTime.fromMillis(currentSrc.time) +
+                                        "\n\t Резервный файл: " + currentDst + "-  " + FileTime.fromMillis(currentDst.time));
+                            }
+                        } else { // если нет резервной копии файла
+                            FileUtils.copyFile(srcPath.toFile(), dstPath.toFile(), true);
+                            Main.LOGGER.log(INFO, srcPath + " не найден и скопирован.");
                         }
-                    } else { // если нет резервной копии файла
-                        FileUtils.copyFile(srcPath.toFile(), dstPath.toFile(), true);
                     }
                 }
+                removeList.add(currentDst);
             }
-            removeList.add(currentDst);
+
+            dst.childList.removeAll(removeList);
+            if (dst.childList.size() != 0) {
+                Main.LOGGER.log(INFO, "в резервном листе остались файлы :" + dst.childList + "которые будут перенесены в корзину");
+                dst.childList.forEach(this::moveOnce);
+            }
+        } catch (IOException e) {
+            Main.LOGGER.log(WARNING, "", e);
+            e.printStackTrace();
         }
-        dst.childList.removeAll(removeList);
-        if (dst.childList.size() != 0) {
-            System.err.println("в резервном листе остались файлы \n" + dst.childList);
-            dst.childList.forEach(myFile -> {
-                try {
-                    FileUtils.forceDelete(myFile.absPath.toFile());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
+    }
 
 
+    private void moveOnce(MyFile dst) {
+        try {
+            FileUtils.moveToDirectory(dst.absPath.toFile(), trash.toFile(), true);
+            Main.LOGGER.log(INFO, "\t" + dst.absPath + " перенесен в корзину");
+
+            File dirInTrashOldName = Paths.get(trash.toString(), dst.path.getFileName().toString()).toFile();
+            StringBuilder newFileName = new StringBuilder();
+            newFileName
+                    .append(FilenameUtils.getBaseName(dst.path.toString()))
+                    .append("_")
+                    .append(new SimpleDateFormat("YYYY.MM.dd_H.mm.s").format(Calendar.getInstance().getTime()));
+            if (!dst.isDir) {
+                newFileName
+                        .append(".")
+                        .append(FilenameUtils.getExtension(dst.path.toString()));
+            }
+
+            File dirInTrashNewName = Paths.get(trash.toString(), newFileName.toString()).toFile();
+
+            boolean renameResult = dirInTrashOldName.renameTo(dirInTrashNewName);
+            Main.LOGGER.log(INFO, " rename " + dirInTrashOldName + " to " + dirInTrashNewName + " result: " + renameResult);
+
+        } catch (IOException e) {
+            Main.LOGGER.log(WARNING, "", e);
+            e.printStackTrace();
         }
-
     }
 
     public int containByName(List<MyFile> another) {
@@ -117,20 +153,8 @@ public class MyFile implements Serializable, Comparable {
         return -1;
     }
 
-    public boolean isDir() {
-        return isDir;
-    }
-
     public boolean isNewer(MyFile another) {
         return time > another.time;
-    }
-
-    public boolean isOlder(MyFile another) {
-        return time < another.time;
-    }
-
-    public List<MyFile> getChildList() {
-        return childList;
     }
 
     @Override
@@ -161,97 +185,8 @@ public class MyFile implements Serializable, Comparable {
         return path.toString();
     }
 
-    public void setChildList(List<MyFile> childList) {
-        this.childList = childList;
-    }
-
-    public long getTime() {
-        return time;
-    }
-
-    public void setPath(Path path) {
-        this.path = path;
-    }
-
     public Path getPath() {
         return path;
     }
 
-
-//
-//    //TODO структура данных с вложением в MyFile всех внутренностей
-//    public static List<MyFile> getFileTreePaths(Path path, boolean isAbsoluteTree) throws Exception {
-//
-//        List<MyFile> myFiles = new ArrayList<>();
-//        Files.walkFileTree(path, new FileVisitor<Path>() {
-//            @Override
-//            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-//                if (dir.equals(path)) return FileVisitResult.CONTINUE;
-//                if (isAbsoluteTree) myFiles.add(new MyFile(dir));
-//                else myFiles.add(new MyFile(dir, path));
-//
-//                return FileVisitResult.CONTINUE;
-//            }
-//
-//            @Override
-//            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-//                if (isAbsoluteTree)
-//                    myFiles.add(new MyFile(file));
-//                else myFiles.add(new MyFile(file, path));
-//                return FileVisitResult.CONTINUE;
-//            }
-//
-//            @Override
-//            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-//                return FileVisitResult.CONTINUE;
-//            }
-//
-//            @Override
-//            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-//                return FileVisitResult.CONTINUE;
-//            }
-//        });
-//        return myFiles;
-//    }
 }
-
-//    public MyFile(Map<String, Object> attributeMap, Path path) {
-//        this.attributeMap = attributeMap;
-//        this.path = path.toAbsolutePath();
-//    }
-//
-//    public MyFile(Path path) throws IOException {
-//        this.attributeMap = Files.readAttributes(path, "*", LinkOption.NOFOLLOW_LINKS);
-//        this.path = path.toAbsolutePath();
-//    }
-//
-//    public MyFile(Path path, Path root) throws IOException {
-//        this.attributeMap = Files.readAttributes(path, "*", LinkOption.NOFOLLOW_LINKS);
-//        this.path = root.toAbsolutePath().relativize(path.toAbsolutePath());
-//    }
-
-//    @Override
-//    public String toString() {
-//        StringBuilder builder = new StringBuilder();
-//        builder.append("\nFile: [").append(path).append("]\n");
-//        for (Map.Entry<String, Object> kv : attributeMap.entrySet()) {
-//            builder.append(kv.getKey()).append(" --- ").append(kv.getValue()).append("\n");
-//        }
-//        return builder.toString();
-//    }
-
-//    @Override
-//    public boolean equals(Object obj) {
-//        MyFile myFile = (MyFile) obj;
-//        return this.getPath().equals(myFile.getPath()) &&
-//                (this.getAttributeMap().get("isDirectory").equals(true) || myFile.getAttributeMap().get("isDirectory").equals(true) ||
-//                        (this.getAttributeMap().get("lastModifiedTime").equals(myFile.getAttributeMap().get("lastModifiedTime")) &&
-//                                this.getAttributeMap().get("size").equals(myFile.getAttributeMap().get("size"))
-//                        )
-//                );
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return getPath().hashCode();
-//    }
